13379ac67483186dcf5d543731575d1f1314bc43
acheronfail <acheronfail@gmail.com>
2020-03-29 20:25:57 +1100

    feat: add --max-variance option

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,11 @@
 use clap::Clap;
 use clap::{crate_authors, crate_version};
 
+use crate::git;
+
 #[derive(Clap)]
 #[clap(version = crate_version!(), author = crate_authors!())]
-pub struct Args {
+struct ClapArgs {
   /// A hex string which is the desired prefix of the hash. If this is not
   /// provided then it defaults to "git config --global gash.default".
   pub prefix: Option<String>,
@@ -13,8 +15,50 @@ pub struct Args {
   #[clap(short = "p", long = "parallel")]
   pub parallel: bool,
 
+  /// The max distance (in seconds) gash can modify the commit times.
+  /// Defaults to one hour.
+  #[clap(short = "m", long = "max-variance", default_value = "3600")]
+  pub max_variance: i64,
+
   /// Whether or not to perform a dry run. This won't create a new repository,
   /// it will just run log out the generated pattern.
   #[clap(short = "d", long = "dry-run")]
   pub dry_run: bool,
+}
+
+/// Public interface for `ClapArgs` which parses some values.
+pub struct Args {
+  pub prefix: String,
+  pub parallel: bool,
+  pub max_variance: i64,
+  pub dry_run: bool,
+}
+
+impl Args {
+  pub fn parse() -> Args {
+    let args = ClapArgs::parse();
+
+    let prefix = match args.prefix {
+      Some(prefix) => prefix,
+      None => git(&["config", "gash.default"])
+        .expect("No prefix given and no value set for gash.default in git config"),
+    };
+
+    let parallel = match args.parallel {
+      // If set via CLI, then honour that.
+      true => true,
+      // Otherwise, try and read git config
+      false => match git(&["config", "gash.parallel"]) {
+        Ok(s) => s == "true",
+        _ => false,
+      },
+    };
+
+    Args {
+      prefix,
+      parallel,
+      dry_run: args.dry_run,
+      max_variance: args.max_variance,
+    }
+  }
 }
