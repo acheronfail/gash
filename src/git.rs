fae1359c2e4c03a0ed579228194c548988df13ec
acheronfail <acheronfail@gmail.com>
2021-06-02 20:41:45 +1000

    fix: find git root and ensure hooks dir exists before adding hook

--- src/git.rs
+++ src/git.rs
@@ -1,8 +1,8 @@
 use std::error::Error;
 use std::fmt::{self, Display, Formatter};
 use std::fs::{self, OpenOptions};
-use std::io::{self, Write};
-use std::path::Path;
+use std::io::Write;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
 #[derive(Debug)]
@@ -59,29 +59,56 @@ pub fn config(name: &str) -> Option<String> {
     run(&["config", name]).ok()
 }
 
+/// Get the root of the current git repository.
+fn root() -> Option<PathBuf> {
+    run(&["rev-parse", "--show-toplevel"])
+        .ok()
+        .map(|s| PathBuf::from(s))
+}
+
 /// Create a simple post-commit hook that runs `gash`.
-pub fn create_post_commit_hook() -> io::Result<()> {
-    let post_commit_hook = ".git/hooks/post-commit";
-    println!("Adding git hook to {}", post_commit_hook);
+pub fn create_post_commit_hook() -> Result<(), GitError> {
+    // Find git root.
+    let git_root = match root() {
+        Some(root) => root,
+        None => {
+            return Err(GitError::new(
+                "Failed to find git root! Are you in a git repository?",
+            ))
+        }
+    };
+
+    let hooks_dir = git_root.join(".git").join("hooks");
+    let post_commit_hook = hooks_dir.join("post-commit");
+    println!("Adding git hook to {}", post_commit_hook.display());
+
+    // Create directories.
+    fs::create_dir_all(&hooks_dir).map_err(|e| GitError::new(format!("{}", e)))?;
 
     // Create file.
     let mut file = OpenOptions::new()
         .create(true)
         .write(true)
         .truncate(true)
-        .open(post_commit_hook)?;
+        .open(&post_commit_hook)
+        .map_err(|e| GitError::new(format!("{}", e)))?;
 
     // Make it executable on unix.
     if cfg!(unix) {
         use std::os::unix::fs::PermissionsExt;
 
-        let mut perms = file.metadata()?.permissions();
+        let mut perms = file
+            .metadata()
+            .map_err(|e| GitError::new(format!("{}", e)))?
+            .permissions();
         perms.set_mode(0o755);
-        fs::set_permissions(post_commit_hook, perms)?;
+        fs::set_permissions(post_commit_hook, perms)
+            .map_err(|e| GitError::new(format!("{}", e)))?;
     }
 
     // Write the hook.
-    file.write_fmt(format_args!("{}", "#!/bin/bash\ngash\n"))?;
+    file.write_fmt(format_args!("{}", "#!/bin/bash\ngash\n"))
+        .map_err(|e| GitError::new(format!("{}", e)))?;
 
     Ok(())
 }
